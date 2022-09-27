b5a35b28a6075441a3ddc8b56e50efb8645201d8
yanagitani <takanori.yanagitani@gmail.com>
2022-09-27 11:00:31 +0900

    fix: fewer unused types

--- pkg/archive/aq.go
+++ pkg/archive/aq.go
@@ -1,7 +1,1 @@
 package aq
-
-import (
-	"context"
-)
-
-type NextQueue func(ctx context.Context, previous string) (next string, err error)
--- pkg/archive/name.go
+++ pkg/archive/name.go
@@ -6,4 +6,6 @@ import (
 
 type NameChecker func(unchecked string) (checked string)
 
+// NameCheckerNoCheck is NameChecker which does not check name.
+// Use this for trusted user input(filename).
 var NameCheckerNoCheck NameChecker = fq.Identity[string]
