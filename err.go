a69caf656513385f52e002bd4b69c1dc6b3277c6
yanagitani <takanori.yanagitani@gmail.com>
2022-09-27 13:37:43 +0900

    feat: queue dir size checker

--- err.go
+++ err.go
@@ -41,3 +41,10 @@ func ErrUnwrapOrElse[T, U any](f func(T) (U, error), g func(error) U) func(T) U 
 		return u
 	}
 }
+
+func ErrTryForEach[T any](t T, e error, f func(T) error) error {
+	if nil != e {
+		return e
+	}
+	return f(t)
+}
---
+++ name.go
@@ -0,0 +1,60 @@
+package fsq
+
+import (
+	"context"
+	"fmt"
+	"io/fs"
+	"os"
+)
+
+type QueueFilenameGenerator func(context.Context) (next string, err error)
+
+type QueueDirChecker func(ctx context.Context, dirname string) (string, error)
+
+type QueueDirStatChecker func(stat fs.FileInfo) (fs.FileInfo, error)
+
+func QueueDirStatCheckerNewBySize(max int64) QueueDirStatChecker {
+	return func(stat fs.FileInfo) (fs.FileInfo, error) {
+		var ok bool = stat.Size() <= max
+		return ErrFromBool(
+			ok,
+			func() fs.FileInfo { return stat },
+			func() error { return fmt.Errorf("Too many queues: %v > %v", stat.Size(), max) },
+		)
+	}
+}
+
+func QueueDirCheckerNewStat(schk QueueDirStatChecker) QueueDirChecker {
+	return func(_ context.Context, dirname string) (checked string, err error) {
+		return ComposeErr(
+			os.Stat, // string -> fs.FileInfo, error
+			ComposeErr(
+				schk, // fs.FileInfo -> fs.FileInfo, error
+				IgnoreArg[fs.FileInfo](func() (string, error) { return dirname, nil }),
+			),
+		)(dirname)
+	}
+}
+
+type QueueFilenameBuilder struct {
+	prev string
+	next NextQueue
+}
+
+func QueueFilenameBuilderNew(init string, next NextQueue) QueueFilenameBuilder {
+	return QueueFilenameBuilder{
+		prev: init,
+		next: next,
+	}
+}
+
+func (b *QueueFilenameBuilder) Next(ctx context.Context) (next string, err error) {
+	next, err = b.next(ctx, b.prev)
+	_ = ErrTryForEach(next, err, func(nex string) error {
+		b.prev = nex
+		return nil
+	})
+	return
+}
+
+func (b *QueueFilenameBuilder) ToGenerator() QueueFilenameGenerator { return b.Next }
